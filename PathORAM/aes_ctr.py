5531eea42497e803eb309475a287e8cf8410b6f6
Hayato <do030b@gmail.com>
2016-07-22 19:25:24 +0900

    introduce private method

--- PathORAM/aes_ctr.py
+++ PathORAM/aes_ctr.py
@@ -13,7 +13,7 @@ class AESCTR:
         self.nonce = int.from_bytes(random_generator.read(16), 'big')
         self.count = 0
 
-    def encrypt(self, plaintext):
+    def __encrypt(self, plaintext):
         encryptor = AES.new(self.__key, AES.MODE_CTR, counter=lambda: ((self.nonce ^ self.count).to_bytes(16, 'big')))
         ciphertext = encryptor.encrypt(plaintext)
         self.count += 1
@@ -21,16 +21,16 @@ class AESCTR:
 
     def encrypt_block(self, block):
         c = self.count
-        return [c] + [self.encrypt(d) for d in block]
+        return [c] + [self.__encrypt(d) for d in block]
 
-    def decrypt(self, ciphertext, count):
+    def __decrypt(self, ciphertext, count):
         decryptor = AES.new(self.__key, AES.MODE_CTR, counter=lambda: (self.nonce ^ count).to_bytes(16, 'big'))
         decoded_text = decryptor.decrypt(ciphertext)
         return decoded_text.decode()
 
     def decrypt_block(self, block):
         c = block[0]
-        return [self.decrypt(d, c+i) for i, d in enumerate(block[1:])]
+        return [self.__decrypt(d, c+i) for i, d in enumerate(block[1:])]
 
 
 if __name__ == '__main__':
