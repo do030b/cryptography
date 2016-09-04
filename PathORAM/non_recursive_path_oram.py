5087227fc0dbeb542ee3a016bcfd2dde4534b25d
Hayato <do030b@gmail.com>
2016-09-04 21:31:58 +0900

    rename non-recursive path oram

--- PathORAM/non_recursive_path_oram.py
+++ PathORAM/non_recursive_path_oram.py
@@ -8,11 +8,11 @@ OP = Enum('OP', 'read write')
 
 
 class Server:
-    def __init__(self, N, Z, recursion_size=None):
+    def __init__(self, N, Z):
         self.N = N
         self.L = math.ceil(math.log2(N)) - 1
         self.Z = Z
-        self.T = [Store(self.N, self.Z) for _ in range(self.L if recursion_size is None else recursion_size+1)]
+        self.T = Store(self.N, self.Z)
 
 
 class Store:
@@ -61,46 +61,45 @@ class Client:
     def decrypt_block(self, block):
         return [int(self.cipher.decrypt(v), 2) for v in block ]
 
-    def initialize(self, recuresion_size=0):
-        for i in range(recuresion_size+1):
-            self.Server.T[i].initialize([self.encrypt_block(self.dummy_block)
-                                         for _ in range(self.Server.T[i].total)])
+    def initialize(self):
+        self.Server.T.initialize([self.encrypt_block(self.dummy_block)
+                                  for _ in range(self.Server.T.total)])
 
-    def read_bucket(self, x, l, i):
-        return [self.decrypt_block(block) for block in self.Server.T[i].read_bucket(x, l)]
+    def read_bucket(self, x, l):
+        return [self.decrypt_block(block) for block in self.Server.T.read_bucket(x, l)]
 
-    def write_bucket(self, x, l, blocks, i):
+    def write_bucket(self, x, l, blocks):
         new_bucket = blocks + [self.dummy_block] * (self.Z-len(blocks))
-        self.Server.T[i].write_bucket(x, l, [self.encrypt_block(block) for block in new_bucket])
+        self.Server.T.write_bucket(x, l, [self.encrypt_block(block) for block in new_bucket])
 
-    def access(self, op, identifier, i=0, new_data=None):
-        old_position = self.position_map[identifier]
-        self.position_map[identifier] = random.randint(0, 2**self.L - 1)
+    def access(self, op, data_id, new_data=None):
+        old_position = self.position_map[data_id]
+        self.position_map[data_id] = random.randint(0, 2**self.L - 1)
 
         for l in range(self.L+1):
-            self.stash.extend([b for b in self.read_bucket(old_position, l, i)
+            self.stash.extend([b for b in self.read_bucket(old_position, l)
                                if b[0] in range(1, 2**self.L+1)])
 
         if op == OP.write:
-            self.stash = [b for b in self.stash if b[0] != identifier] + [[identifier, int.from_bytes(new_data, 'big')]]
+            self.stash = [b for b in self.stash if b[0] != data_id] + [[data_id, int.from_bytes(new_data, 'big')]]
 
-        data = [b[1] for b in self.stash if b[0] == identifier][0] if op == OP.read else int.from_bytes(new_data, 'big')
+        data = [b[1] for b in self.stash if b[0] == data_id][0] if op == OP.read else int.from_bytes(new_data, 'big')
 
         for l in range(self.L, -1, -1):
             new_blocks = [b for b in self.stash
-                          if self.Server.T[i].path(old_position, l) == self.Server.T[i].path(self.position_map[b[0]], l)]
+                          if self.Server.T.path(old_position, l) == self.Server.T.path(self.position_map[b[0]], l)]
             new_blocks = new_blocks[0:min(self.Z, len(new_blocks))]
             self.stash = [b for b in self.stash if b not in new_blocks]
-            self.write_bucket(old_position, l, new_blocks, i)
+            self.write_bucket(old_position, l, new_blocks)
 
         return data.to_bytes(math.ceil((len(bin(data))-2)/8), 'big')
 
 
 if __name__ == '__main__':
-    N = 8
+    N = 9
     Z = 4
     L = math.ceil(math.log2(N)) - 1
-    s = Server(N, Z, 0)
+    s = Server(N, Z)
     p = Client(s)
     print(p.access(OP.write, 1, new_data=b"hello_world 1"))
     print(p.access(OP.write, 2, new_data=b"hello_world 2"))
