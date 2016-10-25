575818df24ef7e26236e67badbbcc02b00ec5098
Hayato <do030b@gmail.com>
2016-10-25 16:27:50 +0900

    refactor class Client

--- SSEwithORAM/sse_with_oram.py
+++ SSEwithORAM/sse_with_oram.py
@@ -13,14 +13,18 @@ class Client:
         self.server = server
         self.data_oram = oram.Client(self.server.data_oram)
         self.index_oram = oram.Client(self.server.index_oram)
+        self.num_of_data = None
 
-    def initialize(self, index_list, data_list):
+    def initialize(self, word_list, data_list):
+        self.num_of_data = len(data_list)
+        index_list = [int(''.join([str(int(w in d)) for d in data_list]), 2) for w in word_list]
         self.data_oram.initialize(data_list)
         self.index_oram.initialize(index_list)
 
     def search(self, word_id):
         index = self.index_oram.access(oram.OP.read, word_id)
-        id_list = [i for i, b in enumerate(bin(index)[2:].zfill(index_bit), 1) if int(b) == 1]
+        id_list = [i for i, b in enumerate(bin(index)[2:].zfill(self.num_of_data).ljust(self.data_oram.N, '0'), 1)
+                   if int(b) == 1]
         return [self.data_oram.access(oram.OP.read, w) for w in id_list]
 
 
@@ -35,13 +39,11 @@ if __name__ == '__main__':
     W = ["word1",
          "word2"]
 
-    Index = [int(''.join([str(int(w in d)) for d in D]), 2) for w in W]
-    index_bit = len(D)
     Nd = Ni = 8
 
     s = Server(Nd, Ni)
     c = Client(s)
-    c.initialize(Index, D)
+    c.initialize(W, D)
 
     for w in W:
         print(w, ':', c.search(W.index(w) + 1))
